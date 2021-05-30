193fe6ef08320a31a356690e598d42b00c072f64
Dennis <dennimanadams@gmail.com>
2021-05-30 18:45:51 +0100

    -add routes -create a addusers function that add add users to the database -creates updateusers function that updates users information in the database -create a deleteuser function that deletes users in the database create post get put and delete http methods to handle the afforementioned functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import client from './database/index.js';
-import { getUsers } from './queries/index.js';
+import { getUsers, addUsers, updateUser, deleteUser } from './queries/index.js';
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8800;
 
 app.use(cors());
 app.use(express.json());
@@ -19,14 +19,16 @@ app.get('/', (req, res) => {
 });
 
 //create
-
+app.post('/users', addUsers)
 
 //read
 app.get('/users', getUsers);
 
 //update
-
+app.put('/users/:userid', updateUser)
 //delete
+app.delete('/users/:userid', deleteUser)
+
 app.listen(port, () => {
   console.log(`Server connected at  http://localhost:${port}`);
 });
--- src/queries/index.js
+++ src/queries/index.js
@@ -2,12 +2,45 @@ import client from '../database/index.js';
 
 export const getUsers = async (req, res) => {
   try {
-    const response = await client.query('SELECT * FROM users ORDER BY id ASC');
+    const response = await client.query('SELECT * FROM person ORDER BY id ASC');
 
     if (response) {
       return res.status(200).json({ status: 'success', data: response.rows });
     }
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
   }
 };
+
+export const addUsers = async (req, res) => {
+  try {
+    const { name, email} = req.body
+    const addUserRes = await client.query(`INSERT INTO person (name, email) VALUES ($1, $2) RETURNING *`,
+    [name, email])
+    return res.json({status: 'success', data: addUserRes.rows})
+  } catch (err) {
+    console.log(err.message)
+  }
+} 
+
+export const updateUser = async (req, res) => {
+  const { userid } = req.params
+  const { name, email } = req.body
+  try {
+      await client.query(`UPDATE person SET name = $1, email = $2 WHERE id = $3`,
+            [name, email, userid])
+         return res.json({status: 'User updated successfully!'})
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  const { userid } = req.params
+  try {
+    await client.query(`DELETE FROM person WHERE id = $1`, [userid])
+    return res.json({message: 'Deleted successfully!'})
+  } catch (err) {
+    console.log(err.message)
+  }
+}
